2ab0192a8ac0bf75e3932d8f4bb21c9b0e03f820
Cristiano Abilio <cristianocafr@gmail.com>
2026-08-03 16:03:13 -0300

    feat: doctor dashboard template set up

---
+++ resources/views/doctor/body/breadcrumb.blade.php
@@ -0,0 +1,22 @@
+<div class="breadcrumb-bar">
+    <div class="container">
+        <div class="row align-items-center inner-banner">
+            <div class="col-md-12 col-12 text-center">
+                <nav aria-label="breadcrumb" class="page-breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html"><i class="isax isax-home-15"></i></a></li>
+                        <li class="breadcrumb-item" aria-current="page">Doctor</li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                    <h2 class="breadcrumb-title">Dashboard</h2>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div class="breadcrumb-bg">
+        <img src="{{ asset('backend/assets/img/bg/breadcrumb-bg-01.png') }}" alt="img" class="breadcrumb-bg-01">
+        <img src="{{ asset('backend/assets/img/bg/breadcrumb-bg-02.png') }}" alt="img" class="breadcrumb-bg-02">
+        <img src="{{ asset('backend/assets/img/bg/breadcrumb-icon.png') }}" alt="img" class="breadcrumb-bg-03">
+        <img src="{{ asset('backend/assets/img/bg/breadcrumb-icon.png') }}" alt="img" class="breadcrumb-bg-04">
+    </div>
+</div>
